840d6bd888ba91e727a9477a6ba10d7702bef8b9
Moritz <moritzb@magenta.de>
2022-12-15 18:37:44 +0100

    tank --> not instant full
    can now decide how much should be filled with fuel

--- src/carrent/CarRent.java
+++ src/carrent/CarRent.java
@@ -26,27 +26,34 @@ public class CarRent {
         System.out.println("Aktuelles Tankvolumen: ");
         int pkwTankvolumen = Integer.parseInt(scanner.nextLine());
 
-        Pkw pkw = new Pkw(pkwName, pkwGrundtarif, true, pkwTuerenanzahl, pkwGetriebe, pkwTankvolumen, pkwMaxTankvolumen);
+        Pkw pkw = new Pkw(pkwName, pkwGrundtarif, true, pkwTuerenanzahl, pkwGetriebe, pkwMaxTankvolumen, pkwTankvolumen);
         System.out.println(pkw);
 
         System.out.println("Wollen Sie den PKW tanken?");
         boolean input = Boolean.parseBoolean(scanner.nextLine());
 
-        if (input == true) {
-            int full = pkw.getMaxTank();
-            int old = pkw.getTank();
-            int difference = full - old;
-            
-            if (pkw.getTank() < pkw.getMaxTank()) {
-                System.out.println("Sie haben " + difference + " Liter getankt.");
-                pkw.setTank(pkw.getMaxTank());
-                
-                System.out.println("---");
-                System.out.println(pkw);
+        double full = pkw.getTankMax();
+        double old = pkw.getTank();
+        double difference = full - old;
+        boolean state = true;
+
+        while (state) {
+            if (input == true) {
+                System.out.println("Wie viel Liter wollen Sie tanken?");
+                double pkwNeuTank = Double.parseDouble(scanner.nextLine());
+
+                if (pkwNeuTank > 100 || pkwNeuTank > difference || pkwNeuTank < 1) {
+                    System.out.println("Sie können nur 1-" + difference + " Liter tanken!");
+                    state = true;
+                } else if (pkwNeuTank < 100 || pkwNeuTank < difference || pkwNeuTank > 1) {
+                    pkw.setTank(old + pkwNeuTank);
+                    System.out.println("Sie haben " + pkwNeuTank + " Liter getankt.");
+                    System.out.println("Ihr Tankvolumen beträgt aktuell: " + pkw.getTank() + " Liter.");
+                    state = false;
+                }
+
             }
 
-        } else {
-            System.out.println(pkw);
         }
 
     }
