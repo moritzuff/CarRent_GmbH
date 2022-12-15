529bfd3ff5d59f5bda36207c09d55594545fa183
schueler.gast <schueler.gast@R5028-PC23.Technik.BTA.local>
2022-12-15 11:52:08 +0100

    Schul-PC Test

--- src/carrent/CarRent.java
+++ src/carrent/CarRent.java
@@ -11,65 +11,8 @@ public class CarRent {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Name: ");
-            String pkwName = scanner.nextLine();
-
-            System.out.println("Grundtarif: ");
-            int pkwGrundtarif = Integer.parseInt(scanner.nextLine());
-
-            System.out.println("Tuerenanzahl: ");
-            int pkwTuerenanzahl = Integer.parseInt(scanner.nextLine());
-
-            System.out.println("Getriebe: ");
-            String pkwGetriebe = scanner.nextLine();
-
-            Pkw tmp = new Pkw(pkwName, pkwGrundtarif, true, pkwTuerenanzahl, pkwGetriebe);
-
-            switch (i) {
-                case 0:
-                    p1 = tmp;
-                    break;
-                case 1:
-                    p2 = tmp;
-                    break;
-                case 2:
-                    p3 = tmp;
-                    break;
-            }
-        }
-
-        System.out.println("Folgende PKWs wurden erstellt: ");
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
-        
-        System.out.println("");
-        System.out.println("Von welchem PKW willst du den Namen ändern?");
-        String tmpName = scanner.nextLine();
-
-        if (tmpName.equalsIgnoreCase("p1")) {
-            System.out.println("Neuer Name: ");
-            String pkwNameNew = scanner.nextLine();
-            p1.setName(pkwNameNew);
-        } else if (tmpName.equalsIgnoreCase("p2")) {
-            System.out.println("Neuer Name: ");
-            String pkwNameNew = scanner.nextLine();
-            p2.setName(pkwNameNew);
-        } else if (tmpName.equalsIgnoreCase("p3")) {
-            System.out.println("Neuer Name: ");
-            String pkwNameNew = scanner.nextLine();
-            p3.setName(pkwNameNew);
-        } else {
-            System.out.println("Es wurde kein PKW mit dem Namen gefunden.");
-        }
-        
-        System.out.println("Aktualisierte Daten: ");
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
-        System.out.println("github test teams v2");
+        String input = scanner.nextLine();
+       
 
     }
 
